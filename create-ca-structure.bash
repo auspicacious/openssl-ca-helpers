61cd52189db7dc4ced6734868cece01e3a56f6ea
Andrew Todd <at@auspicacious.org>
2018-11-28 15:36:04 +0900

    Working on parameterizing all of the CA file locations.

--- create-ca-structure.bash
+++ create-ca-structure.bash
@@ -7,60 +7,104 @@ set -o xtrace
 
 # Reference https://www.feistyduck.com/library/openssl-cookbook/
 
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd -P )"
+
+out_dir="$base_dir"/out
+
+root_ca_dir="$out_dir"/root-ca
+root_certs_dir="$root_ca_dir"/certs
+root_db_dir="$root_ca_dir"/db
+root_private_dir="$root_ca_dir"/private
+
+root_private_key="$root_private_dir"/root-ca.key
+root_cert="$root_certs_dir"/root-ca.crt
+root_ini="$base_dir"/root-ca.ini
+
+sub_ca_dir="$out_dir"/sub-ca
+sub_certs_dir="$sub_ca_dir"/certs
+sub_db_dir="$sub_ca_dir"/db
+sub_private_dir="$sub_ca_dir"/private
+
+sub_private_key="$sub_private_dir"/sub-ca.key
+sub_cert="$sub_certs_dir"/sub-ca.crt
+sub_ini="$base_dir"/sub-ca.ini
+
+root_ca_csr="$(mktemp rootcsr.XXXXXX)"
+sub_ca_csr="$(mktemp subcsr.XXXXXX)"
+
+errmsg() {
+    local IFS=' '
+    printf '%s\n' "$*" 1>&2
+}
+
+trap_exit() {
+    exit_status="$?"
+    rm -rf "$root_ca_csr" "$sub_ca_csr"
+    if [ $exit_status -ne 0 ]; then
+        errmsg 'Something went wrong during execution.'
+        errmsg 'Exit status was: '"$exit_status"
+    fi
+}
+trap EXIT trap_exit
+
 create_root_ca_csr() {
     openssl req \
             -new \
-            -config root-ca.ini \
-            -out root-ca.csr \
-            -keyout private/root-ca.key
+            -config "$root_ini" \
+            -out "$root_ca_csr" \
+            -keyout "$root_private_key"
 }
 
 create_sub_ca_csr() {
     openssl req \
             -new \
-            -config sub-ca.ini \
-            -out sub-ca.csr \
-            -keyout private/sub-ca.key
+            -config "$sub_ini" \
+            -out "$sub_ca_csr" \
+            -keyout "$sub_private_key"
 }
 
 self_sign_root_ca() {
     openssl ca \
             -selfsign \
-            -config root-ca.ini \
-            -in root-ca.csr \
-            -out root-ca.crt \
+            -config "$root_ini" \
+            -in "$root_ca_csr" \
+            -out "$root_cert" \
             -extensions ca_ext
 }
 
 generate_crl() {
     openssl ca \
             -gencrl \
-            -config root-ca.ini \
+            -config "$root_ini" \
             -out root-ca.crl
 }
 
 sign_sub_ca() {
     openssl ca \
-            -config root-ca.ini \
-            -in sub-ca.csr \
-            -out sub-ca.crt \
+            -config "$root_ini" \
+            -in "$sub_ca_csr" \
+            -out "$sub_cert" \
             -extensions sub_ca_ext
 }
 
 make_ca_dirs() {
-    mkdir -p \
-          root-ca/certs \
-          root-ca/db \
-          root-ca/private \
-          sub-ca/certs \
-          sub-ca/db \
-          sub-ca/private
-    chmod 700 root-ca/private sub-ca/private
-    touch root-ca/db/index sub-ca/db/index
-    openssl rand -hex 16 > root-ca/db/serial
-    openssl rand -hex 16 > sub-ca/db/serial
-    printf '%s\n' '1001' > root-ca/db/crlnumber
-    printf '%s\n' '1001' > sub-ca/db/crlnumber
+    mkdir \
+        "$out_dir" \
+        "$root_ca_dir" \
+        "$root_certs_dir" \
+        "$root_db_dir" \
+        "$sub_ca_dir" \
+        "$sub_certs_dir" \
+        "$sub_db_dir"
+    mkdir --mode=700 \
+          "$root_private_dir" \
+          "$sub_private_dir"
+
+    touch "$root_db_dir"/index "$sub_db_dir"/index
+    openssl rand -hex 16 > "$root_db_dir"/serial
+    openssl rand -hex 16 > "$sub_db_dir"/serial
+    printf '%s\n' '1001' > "$root_db_dir"/crlnumber
+    printf '%s\n' '1001' > "$sub_db_dir"/crlnumber
 }
 
 make_ca_dirs
